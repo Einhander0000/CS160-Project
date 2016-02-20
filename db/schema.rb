c87a8031f0f7e1d009a4303781b566e03b1b1508
monsonjeremy <monson.jeremy@gmail.com>
2016-02-19 23:10:37 -0800

    stash

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160219200720) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "signups", force: :cascade do |t|
     t.string   "screen_name"
     t.string   "first_name"
